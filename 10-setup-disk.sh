c681c8d6c2781f718f417fceb0d5c78c114b9222
dkrm <git@dkrm.fastmail.com>
2019-03-19 16:22:26 +0100

    add script to partition/format LFS disk

---
+++ 10-setup-disk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+LFS_DEVICE='/dev/sdb'
+LFS_PATH='/mnt/lfs'
+pacman -Sy --needed --noconfirm parted
+
+umount --quiet --recursive ${LFS_PATH} || true
+
+parted --script --align minimal ${LFS_DEVICE} -- \
+       mklabel msdos \
+       mkpart primary ext2 1MiB 1GiB \
+       set 1 boot on \
+       mkpart primary linux-swap 1GiB 3GiB \
+       mkpart primary ext4 3GiB 100%
+
+mkfs.ext2 -L boot /dev/sdb1
+mkswap    -L swap /dev/sdb2
+mkfs.ext4 -L lfs  /dev/sdb3
+
+mkdir -p ${LFS_PATH}      && mount /dev/sdb3 ${LFS_PATH}
+mkdir -p ${LFS_PATH}/boot && mount /dev/sdb1 ${LFS_PATH}/boot
