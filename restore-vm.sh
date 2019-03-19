c8b5200b16bdf629080b9ca2845701a27d8d8798
dkrm <git@dkrm.fastmail.com>
2019-03-19 16:23:55 +0100

    remove (restore|save)-vm.sh

--- restore-vm.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-rsync -Pv /sgoinfre/goinfre/Perso/djean/ft_linux_lfs.vdi /Volumes/Storage/goinfre/djean/
-echo "VM restored to local storage"
--- save-vm.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-rsync -Pv /Volumes/Storage/goinfre/djean/ft_linux_lfs.vdi /sgoinfre/goinfre/Perso/djean/
-echo "VM saved into NFS"
